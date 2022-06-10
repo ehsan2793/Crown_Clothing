a9200d0a4d1b62ac19564e5b5204991d5ab3ebf5
Ehsan Rahimi <ehsanrahimi@Ehsans-MacBook-Pro.local>
2022-06-10 10:08:27 -0700

    created Homepage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
+import "./App.css";
 
-import './App.css';
-
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import HomePage from "./HomePage";
 
 class App extends Component {
   render() {
-    return <div className='App'>Hello World</div>;
+    return (
+      <div>
+        <HomePage />
+      </div>
+    );
   }
 }
 
---
+++ src/HomePage.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+
+import "./homepage.styles.scss";
+
+const HomePage = () => {
+  return (
+    <div className="homepage">
+      <div className="directory-menu">
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">hat</h1>
+            <span className="subtitle">shop</span>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">hat</h1>
+            <span className="subtitle">shop</span>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">hat</h1>
+            <span className="subtitle">shop</span>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">hat</h1>
+            <span className="subtitle">shop</span>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">hat</h1>
+            <span className="subtitle">shop</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
