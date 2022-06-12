3fdd7e950e5ef2406325bf6fc2dde0b299bf7509
Ehsan Rahimi <ehsanrahimi@Ehsans-MacBook-Pro.local>
2022-06-11 23:55:00 -0700

    sign up added

--- src/pages/sign-in - sign-up/SignInPage.jsx
+++ src/pages/sign-in - sign-up/SignInPage.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 
 import SignIn from "../../components/sign-in/SignIn";
+import SignUp from "../../components/sign-up/SignUp";
 import "./sign-in-page.scss";
 
 const SignInPage = () => {
   return (
-    <div>
+    <div className="sign-in-up">
       <SignIn />
+      <SignUp />
     </div>
   );
 };
